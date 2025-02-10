b520b0fe6b5c902efa7f1c9b7d82b0ef15434597
Dung-vnext <dungnt2@vnext.vn>
2025-02-10 15:09:59 +0700

    Add files via upload

---
+++ helloworld/index.php
@@ -0,0 +1,18 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>
+        <?php 
+        echo "Hello, World!"; 
+        ?> </h1>
+        <p>Welcome to your first PHP project!</p>
+    </div>
+</body>
+</html>
